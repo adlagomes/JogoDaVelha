87e831e13c3777b5da7b4f45d2ec51149c1018fc
Adla Gomes <adlagomes@gmail.com>
2021-08-30 16:07:55 -0300

    Iniciando script

---
+++ script.js
@@ -0,0 +1,12 @@
+var jogador = "x"
+var jogo = [] // Array que irá guardar e verificar os posições jogadas
+var tabuleiro = [] // Array que irá controlar/atualizar os elementos visuais
+var quemJoga = 0
+var verificarVitoria;
+var partidaIniciada = true
+var turnoCpu = 1
+var quemComeca = 1
+
+function turnoJogador(p) {
+    
+}
